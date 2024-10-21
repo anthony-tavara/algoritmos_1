d1da3ca6e3ed0a9a9f4966ba8eb5640c541be74e
Anthony Távara <atavara@fi.uba.ar>
2024-10-21 13:54:15 -0300

    Actualizo ej2

--- guia_programas_modulares/ej2_capicua.c
+++ guia_programas_modulares/ej2_capicua.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 #define MF 10
 typedef int t_vec[MF];
 
-void devolver_capicua(t_vec vector,int ml){
+
+bool es_capicua(t_vec vector,int ml){
 	int i;
+	int coincidencias=0;
 	for(i=0;i<ml;i++){
-		printf("%i\n",vector[ml]);		
+		if(vector[i]==vector[ml-i]){
+			coincidencias++;
+		}
 	}
+	return (coincidencias==((ml-1)/2));
 }
 
 void main(){
 	t_vec vector; 
 	int num,pos=0;
 	do{	
+		
 		printf("Ingrese número: ");
 		scanf("%i",&num);
-		printf("%i",num);
-		vector[pos]=num;
-		pos++;
+		if (num!=0){
+			vector[pos]=num;
+			pos++;
+		}
 	}while(num!=0);
-	devolver_capicua(vector,pos);
+	printf("%i",es_capicua(vector,pos));
 }
