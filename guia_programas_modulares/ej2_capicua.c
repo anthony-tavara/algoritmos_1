98ad83d4368702c7e911ac7e5e555eedc96ce58c
Anthony Távara <atavara@fi.uba.ar>
2024-10-21 12:00:42 -0300

    Agrego ej2 semi hecho

--- guia_programas_modulares/ej2_capicua.c
+++ guia_programas_modulares/ej2_capicua.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 
+typedef int t_vec[10];
 
+void devolver_capicua(int t_vec){
+	printf("%i",t_vec);
+}
+
+void main(){
+	t_vec vector; 
+	int num,posicion=0;
+	do{	
+		printf("Ingrese número: ");
+		scanf("%i",&num);
+		vector[posicion]=num;
+		posicion++;
+	}while(num!=0);
+	devolver_capicua(vector);
+
+	
+}
