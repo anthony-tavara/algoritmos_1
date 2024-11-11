7b9baefdec01791396d882ec98783cc5cef354b8
Anthony Távara <atavara@fi.uba.ar>
2024-11-11 20:27:45 -0300

    Arreglo ej3

--- guia_cadenas/ej-3.c
+++ guia_cadenas/ej-3.c
@@ -15,23 +15,28 @@ cant_palabras("      ") //devuelve 0                 */
 #include <stdio.h>
 #include <string.h>
 
-#define MF 20
+#define MF 100
 typedef char t_cadena[MF];
 
 int cant_palabras(t_cadena cadena){
 	int i;
 	int cant_palabras=0;
+	int en_palabra=0;
+	
 	while(cadena[i]!='\0'){
-		if(cadena[i]!=' ' && cadena[i+1]==' ' ){
+		if(cadena[i]!=' ' && !en_palabra){
 			cant_palabras++;
+			en_palabra=1;
 			}
+		if(cadena[i]==' ')
+			en_palabra=0;
 		i++;
 	}
 	return cant_palabras;
 }
 int main(){
 	t_cadena cadena;
-	fgets(cadena,20,stdin);
+	fgets(cadena,50,stdin);
 	fflush(stdin);
 	printf("%i",cant_palabras(cadena)); //devuelve 4
 //	printf("%i",cant_palabras(" Hoy es 30 de Junio de  2021 ")); //devuelve 7
