6c98aca785e02f76085d94d08f1d0f39c3a99ae9
Anthony Távara <atavara@fi.uba.ar>
2024-11-13 23:09:15 -0300

    ej9 finished

--- guia_cadenas/ej-9.c
+++ guia_cadenas/ej-9.c
@@ -1,5 +1,4 @@
-/*
-Escribir una función que recibirá por parámetro, una palabra, que representa un sustantivo en singular.
+/*Escribir una función que recibirá por parámetro, una palabra, que representa un sustantivo en singular.
 La función deberá devolver, el plural de dicho sustantivo, aplicando las siguientes reglas:
 
 - Agregar una 's' al final, si la palabra termica en vocal sin acento.
@@ -8,8 +7,8 @@ La función deberá devolver, el plural de dicho sustantivo, aplicando las sigui
 - Agregamos 'es' al final, si la palabra termina con una consonante (a excepción de la 's', la 'z',
 y la 'x'), ó si la palabra termina con las vocales acentuadas: á,í,ó,ú.
 - Si el sustantivo termina en 's' ó 'x', entonces el plural es igual al singular, por lo tanto
-la función deberá devolver lo mismo que recibió.
-*/
+la función deberá devolver lo mismo que recibió.                                            */
+
 
 #include <stdio.h>
 #include <string.h>
@@ -17,17 +16,42 @@ la función deberá devolver lo mismo que recibió.
 #define MF 30
 typedef char t_cad[MF];
 
-void devolver_plural(t_cad sustantivo){
+void devolver_plural(t_cad sustantivo,t_cad sustantivo_plural){
 	int ml=strlen(sustantivo); //máximo logico ('\0')
-	if( sustantivo[ml-1]=='a' || sustantivo[ml-1]=='e' || sustantivo[ml-1]=='i' || sustantivo[ml-1]=='o' || sustantivo[ml-1]=='u' || sustantivo[ml-1]=='é'){
-		sustantivo[ml]='s';
-		sustantivo[ml+1]='\0';
-	}else if(sustantivo[ml-1]=='z'){
-		sustantivo[ml-1]='c';
-		sustantivo[ml]='e';
-		sustantivo[ml+1]='s';
-		sustantivo[ml+2]='\0';
-		
+
+	if( sustantivo[ml-1]=='a' || sustantivo[ml-1]=='e' || sustantivo[ml-1]=='i' || sustantivo[ml-1]=='o' || sustantivo[ml-1]=='u'){
+		strcat(sustantivo_plural, sustantivo);
+		strcat(sustantivo_plural,"s");
 	}
-		
+	//intente meter el caso de las vocales con acento pero me tira error por alguna razón.
+	else if(sustantivo[ml-1]=='z'){
+		strcat(sustantivo_plural,sustantivo);
+		sustantivo_plural[ml-1]='\0';
+		strcat(sustantivo_plural,"ces");
+	}	
+	else{
+		strcat(sustantivo_plural,sustantivo);		
+		strcat(sustantivo_plural,"es");
+	}
+}
+int main(void){
+	t_cad sustantivo1 = "Casa";
+	t_cad sustantivo2 = "Raiz";
+	t_cad sustantivo3 = "Monitor";
+
+	t_cad sustantivo_plural1;
+	t_cad sustantivo_plural2;
+	t_cad sustantivo_plural3;
+
+	devolver_plural(sustantivo1,sustantivo_plural1);
+	devolver_plural(sustantivo2,sustantivo_plural2);
+	devolver_plural(sustantivo3,sustantivo_plural3);
+	
+	printf("Sustantivo Singular\tSustantivo Plural\n");
+	printf("-----------------------------------------\n");
+	printf("%s\t\t\t%s\n",sustantivo1,sustantivo_plural1);
+	printf("%s\t\t\t%s\n",sustantivo2,sustantivo_plural2);
+	printf("%s\t\t\t%s\n",sustantivo3,sustantivo_plural3);
+
+	return 0;
 }
