3eabdd8d5dd8ed9718f75f21beb8a7fccc8afe23
Anthony Távara <atavara@fi.uba.ar>
2024-11-22 18:59:28 -0300

    ej5 finished

--- guia_recursividad/ej5-mayor-valor.c
+++ guia_recursividad/ej5-mayor-valor.c
@@ -4,25 +4,27 @@ números enteros.
 */
 
 #include <stdio.h>
-#define MF 30
+#define MF 6
 typedef int T_vec[MF];
 
 
-int calcular_mayor_valor(T_vec vector,int pos,int mayor_valor,int ml){
+int calcular_mayor_valor(T_vec vector,int pos,int ml){
 
-	if(pos>ml){
-		return mayor_valor;
-	}
+	if(pos==ml)
+		return vector[pos];
 	else{
-		if(mayor_valor<vector[pos])
-			mayor_valor=vector[pos];
-			
-		return calcular_mayor_valor(vector,pos+1,mayor_valor,ml);			
+		if(vector[pos]>calcular_mayor_valor(vector,pos+1,ml))
+			return vector[pos];
+		else 
+			return calcular_mayor_valor(vector,pos+1,ml);			
 	}
 }
 
 int main(){
-	T_vec vector = {2,50,20,23,40,5};
-	printf("Mayor valor en el vector: %i\n\n",calcular_mayor_valor(vector,0,0,6));
+	T_vec vector = {22,50,200,230,40,23};
+	int ml=6;
+	
+	printf("Mayor valor en el vector: %i\n\n",calcular_mayor_valor(vector,0,ml-1));
+
 	return 0;
 }
