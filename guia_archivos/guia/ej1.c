334e3cb1dfa1d0ef21c0c2513da6342b585a0551
anthony <atavara@fi.uba.ar>
2025-01-19 12:27:20 -0300

    Modificado: Ej1 finalizado

--- guia_archivos/guia/ej1.c
+++ guia_archivos/guia/ej1.c
@@ -9,33 +9,52 @@ correspondiente del archivo de entrada.
 void cargarNumeros(){
 	FILE *archivo;
 	archivo=fopen("ej1.txt","wb");
+	if(archivo==NULL)
+		printf("Archivo no encontrado.");
 	int i;
 	for(i=0;i<11;i++){
 		fwrite(&i,sizeof(int),1,archivo);
 	}
 	fclose(archivo);
 }
+
+
 int calcularFactorial(int num){
 	if(num == 0 || num == 1 )
 		return 1;
-
 	return ( num * calcularFactorial(num-1) );
 }
+void almacenarNum(int num){
+	FILE *archivoSalida;
+	archivoSalida = fopen("ej1-factoriales.txt","ab");
+	if(archivoSalida==NULL)
+		printf("Archivo no encontrado");
+	
+	fwrite(&num,sizeof(int),1,archivoSalida);
+	fclose(archivoSalida);
+}
+
 void leerNumeros(){
 	FILE *archivo;
 	archivo=fopen("ej1.txt","rb");
-	int i;
+	if(archivo==NULL)
+		printf("Archivo no encontrado");
+	int i=0;
 	int num;
 	fread(&num,sizeof(int),1,archivo);
 
-	for(i=0;i<11;i++){
-		printf("El factorial de %i es: %i.\n",num,calcularFactorial(num));
+	while(!feof(archivo)){
+		int factorial = calcularFactorial(num);
+		printf("El factorial de %i es: %i.\n",i,factorial);	
+		almacenarNum(factorial);
+		i++;
 
 		fread(&num,sizeof(int),1,archivo);
 	}
 	fclose(archivo);
 }
+
 int main(void){
-	//cargarNumeros();
+	cargarNumeros();
 	leerNumeros();
 }
